974a1dd9874414cb52f445fda1be9808c5cfe617
Sbax <heysbax@protonmail.com>
2021-11-19 23:37:35 +0100

    Fixed class

--- components/checkbox.js
+++ components/checkbox.js
@@ -3,7 +3,7 @@ import checkboxStyle from "../styles/checkbox.module.scss";
 
 export default function Checkbox({ children }) {
   return (
-    <label class={checkboxStyle.checkbox}>
+    <label className={checkboxStyle.checkbox}>
       <input type="checkbox" name="checkbox" />
       {children}
     </label>
