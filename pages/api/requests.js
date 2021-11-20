38102b7cdb1f39e04bda2036726a45fde0360cfc
Sbax <heysbax@protonmail.com>
2021-11-20 10:00:57 +0100

    Removed debugging logs

--- pages/api/requests.js
+++ pages/api/requests.js
@@ -7,14 +7,6 @@ export default async function handler(req, res) {
     handleResponse({ res }, await addRequest(req.body), async () => {
       const { event, name, people, instagram, phone } = req.body;
 
-      console.log("Handling response", {
-        event,
-        name,
-        people,
-        instagram,
-        phone,
-      });
-
       const message = [
         `Nuova prenotazione per ${event.title} in data ${event.date} con ${event.dm}`,
         `${name}, ${people} persone (instagram: ${instagram}, phone: ${phone})`,
