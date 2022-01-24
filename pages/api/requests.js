41cb3c0000362914b425801675d74199a668a8b2
Sbax <heysbax@protonmail.com>
2022-01-24 14:38:40 +0100

    Fixed bot message

--- pages/api/requests.js
+++ pages/api/requests.js
@@ -8,7 +8,7 @@ export default async function handler(req, res) {
       const { event, name, people, instagram, phone } = req.body;
 
       const message = [
-        `Nuova prenotazione per ${event.title} in data ${event.date} con ${event.dm}`,
+        `Nuova prenotazione per ${event.title} (${event.system}) in data ${event.date} con ${event.dm.name}`,
         `${name}, ${people} persone (instagram: ${instagram}, phone: ${phone})`,
         "",
         `<a href="https://docs.google.com/spreadsheets/d/${process.env.GOOGLE_SPREADSHEET_ID_FROM_URL}">Google Sheet</a>`,
