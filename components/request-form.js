4bbd1f733256c4106d69444542d71759cd4dbac6
Sbax <heysbax@protonmail.com>
2022-07-04 08:42:55 +0200

    Blocked requests in some cases

--- components/request-form.js
+++ components/request-form.js
@@ -10,7 +10,8 @@ import Loader from "./loader";
 const FormState = { Idle: 0, Sending: 1, Success: 2, Error: 3 };
 
 export default function RequestForm({ event }) {
-  const { title, system, dm, date, time, place, max, booked } = event;
+  const { title, system, dm, date, time, place, max, booked, reservationLink } =
+    event;
   const [people, setPeople] = useState({ value: 1 });
   const [name, setName] = useState({ value: "" });
   const [phone, setPhone] = useState({ value: "" });
@@ -73,6 +74,197 @@ export default function RequestForm({ event }) {
     setFormState(FormState.Success);
   }, [event, people, name, phone, instagram, remainingPlaces]);
 
+  const Content = () => {
+    if (remainingPlaces <= 0) {
+      return (
+        <>
+          <h1>Tutti i posti sono prenotati!</h1>
+          <Link href="/events">
+            <a>
+              <h2>Cerca un&apos;altra sessione tra i nostri eventi!</h2>
+            </a>
+          </Link>
+        </>
+      );
+    }
+
+    if (new Date(date) < new Date()) {
+      return (
+        <>
+          <h1>La sessione è nel passato!</h1>
+          <Link href="/events">
+            <a>
+              <h2>Cerca un&apos;altra sessione tra i nostri eventi!</h2>
+            </a>
+          </Link>
+        </>
+      );
+    }
+
+    if (reservationLink) {
+      return (
+        <>
+          <h1>Le prenotazioni sono gestite esternamente!</h1>
+          <Link href={reservationLink}>
+            <a>
+              <h2>Prenotati cliccando qui!</h2>
+            </a>
+          </Link>
+        </>
+      );
+    }
+
+    return (
+      <>
+        <form className={requestFormStyles.form}>
+          {remainingPlaces !== 1 && (
+            <section className={requestFormStyles.item}>
+              <label>Quante persone?</label>
+              <Input
+                disabled={formState === FormState.Success}
+                value={people.value}
+                error={people.error}
+                onChange={({ target }) => {
+                  const { value } = target;
+
+                  if (!value || value < 1 || value > remainingPlaces) {
+                    return setPeople({ value: target.value, error: true });
+                  }
+
+                  setPeople({ value: target.value });
+                }}
+                id="people"
+                type="number"
+                placeholder={`1-${remainingPlaces}`}
+                min="1"
+                max={remainingPlaces}
+              />
+            </section>
+          )}
+
+          <section className={requestFormStyles.item}>
+            <label>Nome</label>
+            <Input
+              disabled={formState === FormState.Success}
+              value={name.value}
+              error={name.error}
+              onChange={({ target }) =>
+                setName({
+                  value: target.value,
+                  error: !target.value || target.value === "",
+                })
+              }
+              id="name"
+              type="text"
+              placeholder="Nome"
+            />
+          </section>
+
+          <section className={requestFormStyles.item}>
+            <label>Numero di telefono</label>
+            <Input
+              disabled={formState === FormState.Success}
+              value={phone.value}
+              error={phone.error}
+              onChange={({ target }) => {
+                const { value } = target;
+                const hasError = !value || value === "";
+
+                const instagramError =
+                  !instagram.value || instagram.value === "";
+
+                setInstagram({
+                  value: instagram.value,
+                  error: hasError && instagramError,
+                });
+
+                setPhone({
+                  value: value,
+                  error: hasError && instagramError,
+                });
+              }}
+              id="phone"
+              type="text"
+              placeholder="XXX 123 45 67"
+            />
+          </section>
+
+          <section className={requestFormStyles.item}>
+            <label>Instagram</label>
+            <Input
+              disabled={formState === FormState.Success}
+              value={instagram.value}
+              error={instagram.error}
+              onChange={({ target }) => {
+                const { value } = target;
+                const hasError = !value || value === "";
+                const phoneError = !phone.value || phone.value === "";
+
+                setPhone({
+                  value: phone.value,
+                  error: hasError && phoneError,
+                });
+
+                setInstagram({
+                  value: value,
+                  error: hasError && phoneError,
+                });
+              }}
+              id="instagram"
+              type="text"
+              placeholder="username"
+            />
+          </section>
+          <section>
+            <Checkbox
+              value={privacyAccepted}
+              onChange={() => setPrivacyAccepted(!privacyAccepted)}
+            >
+              <span className={requestFormStyles.privacy}>
+                Ho letto la <Link href="/privacy-policy">privacy policy</Link> e
+                acconsento al trattamento dei dati che non verranno ceduti a
+                terzi
+              </span>
+            </Checkbox>
+          </section>
+        </form>
+        <section className={requestFormStyles.button}>
+          <Button
+            disabled={
+              formState === FormState.Success ||
+              name.error ||
+              instagram.error ||
+              phone.error ||
+              !privacyAccepted
+            }
+            secondary
+            onClick={() => sendForm()}
+          >
+            Prenota!
+          </Button>
+        </section>
+        {formState === FormState.Success && (
+          <section className={requestFormStyles.success}>
+            <h2>
+              La richiesta è stata inviata, tra poco riceverai un messaggio per
+              confermarla!
+            </h2>
+
+            <p>
+              Non si tratta di un processo automatico, perciò sii paziente! I
+              nostri coboldi ti risponderanno al più presto!
+            </p>
+          </section>
+        )}
+        {formState === FormState.Error && (
+          <section className={requestFormStyles.error}>
+            <h2>Si è verificato un errore, riprova.</h2>
+          </section>
+        )}
+      </>
+    );
+  };
+
   return (
     <section className={requestFormStyles.container}>
       {formState === FormState.Sending ? (
@@ -89,151 +281,7 @@ export default function RequestForm({ event }) {
         {system} con {dm.name}, il <DateFormatter dateString={date} /> alle{" "}
         {time} presso <a href={place.link}>{place.name}</a>
       </h2>
-      <form className={requestFormStyles.form}>
-        {remainingPlaces !== 1 && (
-          <section className={requestFormStyles.item}>
-            <label>Quante persone?</label>
-            <Input
-              disabled={formState === FormState.Success}
-              value={people.value}
-              error={people.error}
-              onChange={({ target }) => {
-                const { value } = target;
-
-                if (!value || value < 1 || value > remainingPlaces) {
-                  return setPeople({ value: target.value, error: true });
-                }
-
-                setPeople({ value: target.value });
-              }}
-              id="people"
-              type="number"
-              placeholder={`1-${remainingPlaces}`}
-              min="1"
-              max={remainingPlaces}
-            />
-          </section>
-        )}
-
-        <section className={requestFormStyles.item}>
-          <label>Nome</label>
-          <Input
-            disabled={formState === FormState.Success}
-            value={name.value}
-            error={name.error}
-            onChange={({ target }) =>
-              setName({
-                value: target.value,
-                error: !target.value || target.value === "",
-              })
-            }
-            id="name"
-            type="text"
-            placeholder="Nome"
-          />
-        </section>
-
-        <section className={requestFormStyles.item}>
-          <label>Numero di telefono</label>
-          <Input
-            disabled={formState === FormState.Success}
-            value={phone.value}
-            error={phone.error}
-            onChange={({ target }) => {
-              const { value } = target;
-              const hasError = !value || value === "";
-
-              const instagramError = !instagram.value || instagram.value === "";
-
-              setInstagram({
-                value: instagram.value,
-                error: hasError && instagramError,
-              });
-
-              setPhone({
-                value: value,
-                error: hasError && instagramError,
-              });
-            }}
-            id="phone"
-            type="text"
-            placeholder="XXX 123 45 67"
-          />
-        </section>
-
-        <section className={requestFormStyles.item}>
-          <label>Instagram</label>
-          <Input
-            disabled={formState === FormState.Success}
-            value={instagram.value}
-            error={instagram.error}
-            onChange={({ target }) => {
-              const { value } = target;
-              const hasError = !value || value === "";
-              const phoneError = !phone.value || phone.value === "";
-
-              setPhone({
-                value: phone.value,
-                error: hasError && phoneError,
-              });
-
-              setInstagram({
-                value: value,
-                error: hasError && phoneError,
-              });
-            }}
-            id="instagram"
-            type="text"
-            placeholder="username"
-          />
-        </section>
-        <section>
-          <Checkbox
-            value={privacyAccepted}
-            onChange={() => setPrivacyAccepted(!privacyAccepted)}
-          >
-            <span className={requestFormStyles.privacy}>
-              Ho letto la <Link href="/privacy-policy">privacy policy</Link> e
-              acconsento al trattamento dei dati che non verranno ceduti a terzi
-            </span>
-          </Checkbox>
-        </section>
-      </form>
-      <section className={requestFormStyles.button}>
-        <Button
-          disabled={
-            formState === FormState.Success ||
-            name.error ||
-            instagram.error ||
-            phone.error ||
-            !privacyAccepted
-          }
-          secondary
-          onClick={() => sendForm()}
-        >
-          Prenota!
-        </Button>
-      </section>
-
-      {formState === FormState.Success && (
-        <section className={requestFormStyles.success}>
-          <h2>
-            La richiesta è stata inviata, tra poco riceverai un messaggio per
-            confermarla!
-          </h2>
-
-          <p>
-            Non si tratta di un processo automatico, perciò sii paziente! I
-            nostri coboldi ti risponderanno al più presto!
-          </p>
-        </section>
-      )}
-
-      {formState === FormState.Error && (
-        <section className={requestFormStyles.error}>
-          <h2>Si è verificato un errore, riprova.</h2>
-        </section>
-      )}
+      <Content />
     </section>
   );
 }
